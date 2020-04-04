3a42a9ad49c3952695925fe7cc52d1617dbcadf2
UtkarshChandel <personal.utkarsh24@gmail.com>
2020-04-04 14:01:09 +0530

    Updates

---
+++ precache-manifest.bb986860b9ea898f826df04fa3982d1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57ca83bf8608c5b2fa03bb65cd21d64",
+    "url": "/reactPokemon/index.html"
+  },
+  {
+    "revision": "63f27ed2aa82d9b1c40c",
+    "url": "/reactPokemon/static/css/main.3d98e042.chunk.css"
+  },
+  {
+    "revision": "7b4f9c8a13d9ec03fa13",
+    "url": "/reactPokemon/static/js/2.8f7955f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactPokemon/static/js/2.8f7955f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63f27ed2aa82d9b1c40c",
+    "url": "/reactPokemon/static/js/main.addafd90.chunk.js"
+  },
+  {
+    "revision": "1904f2322a5eb6fa7d2a",
+    "url": "/reactPokemon/static/js/runtime-main.1d69dc3f.js"
+  }
+]);
